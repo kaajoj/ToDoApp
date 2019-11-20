b01196dabdf9b8b37bbc69c066e8a070b2e8e2b0
kaajoj <kaajooj@gmail.com>
2019-11-20 08:09:43 +0100

    Functions showTasks, delete and check added

--- ToDoApp/wwwroot/js/site.js
+++ ToDoApp/wwwroot/js/site.js
@@ -3,18 +3,40 @@
 var list = {};
 
 function addTask() {
-    list = new Array();
-
     var task = {};
     task.name = $("#newTask").val();
 
-    list.push(task);
+    list.tasks.push(task);
     console.info(list);
 //    $("#tasks").empty();
+    showTasks();
+}
+
+function showTasks() {
+    var $list = $("#tasks").empty();
+
+    for (var i = 0; i < list.tasks.length; i++) {
+        var currentTask = list.tasks[i];
+        var $li = $("<li>").html(currentTask.name).attr("id", "item_" + i);
+        var $checkBtn = $("<button onclick='checkTask(" + i + ")'>Check</button>").appendTo($li);
+        var $deleteBtn = $("<button onclick='deleteTask(" + i + ")'>Delete</button>").appendTo($li);
+
+        $li.appendTo($list);
+    }
+}
+
+function deleteTask(id) {
+    list.tasks.splice(id, 1);
+    showTasks();
+}
+
+function checkTask(id) {
+    $("#item_" + id).addClass("checked");
 }
 
 $(document).ready(function() {
     console.info("1ready");
+    list.tasks = new Array();
 //    console.log("2ready");
 //    console.error("3ready");
 });
